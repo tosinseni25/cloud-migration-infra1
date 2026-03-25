573a767e5c4450d63e1792ba1c84da61034f4d25
fda2026 <jamil.ceo.abiodun@gmail.com>
2026-03-25 13:49:28 +0100

    Adding helm uninstall

--- helm/prometheus.sh
+++ helm/prometheus.sh
@@ -5,6 +5,7 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 #helm install prometheus prometheus-community/prometheus \
+helm uninstall monitoring prometheus-community/kube-prometheus-stack \
 helm install monitoring prometheus-community/kube-prometheus-stack \
 -f monitoring/prometheus-values.yaml \
 -f monitoring/values.yaml \
